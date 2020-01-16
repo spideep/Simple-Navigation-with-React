3c1b6a74bdc136f7bdf5798688005879b804a052
Alexis Rengifo <alexisrengifo@gmail.com>
2020-01-16 15:30:32 -0500

    Simple Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import Menu from './components/Menu/Menu';
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+import WorkWithMe from './components/pages/WorkWithMe';
 
 function App() {
   return (
@@ -19,6 +26,19 @@ function App() {
           Learn React
         </a>
       </header>
+      <BrowserRouter>
+        <div>
+          <Menu layout="horizontal" />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/Contact" component={Contact} />
+            <Route path="/WorkWithMe" component={WorkWithMe} />
+            <Route path="/Projects" component={Projects} />
+            <Route path="/Contact" component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/pages/WorkWithMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WorkWithMe = () => {
+  return (
+    <div>
+      <h1>WorkWithMe</h1>
+      <p>WorkWithMe page body content</p>
+    </div>
+  );
+}
+
+export default WorkWithMe;
