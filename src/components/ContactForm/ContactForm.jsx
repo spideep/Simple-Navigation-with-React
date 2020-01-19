f8f03ff03271ec54ab527c3755b954d7abad0d0e
Alexis Rengifo <alexisrengifo@gmail.com>
2020-01-19 03:55:42 -0500

    useState hook usage for set state variables.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './ContactForm.scss';
 
-const ContactForm = () => (
-  <form id="contact-form" action="http://spideep.com/test_sendmail/index.php" onSubmit={handleSubmit} method="POST" className="ContactForm">
-    <div className="form-group">
-      <label htmlFor="name">Name</label>
-      <input name="name" type="text" className="form-control" />
-    </div>
-    <div className="form-group">
-      <label htmlFor="exampleInputEmail1">Email</label>
-      <input type="email" name="email" className="form-control" aria-describedby="emailHelp" />
-    </div>
-    <div className="form-group">
-      <label htmlFor="message">Message</label>
-      <textarea name="message" className="form-control" rows="5"></textarea>
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </form>
-);
 
-const handleSubmit = what => {
-  // what.preventDefault();
-  what.preventDefault();
-  console.log('what', what);
+const ContactForm = () => {
+  const [name, setName] = useState('name...');
+  const [message, setMessage] = useState('message...');
+  const [email, setEmail] = useState('email...');
+
+  const handleSubmit = what => {
+    what.preventDefault();
+    console.log('what', what);
+  };
+
+  return (
+    <form id="contact-form" action="http://spideep.com/test_sendmail/index.php" onSubmit={handleSubmit} method="POST" className="ContactForm">
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Email</label>
+        <input 
+          type="email" 
+          name="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)} 
+          className="form-control" 
+          aria-describedby="emailHelp" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea 
+          name="message" 
+          className="form-control"
+          value={message}
+          onChange={setMessage}
+          rows="5">
+          </textarea>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+  )
 };
+
+
 
 ContactForm.propTypes = {};
 
