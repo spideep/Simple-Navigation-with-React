3817dc2dad6eb39f60bffb8b2a8c9961f2a2e332
Alexis Rengifo <alexisrengifo@gmail.com>
2020-01-23 01:18:48 -0500

    Gallery passing api_key, user_id, limit

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,12 +4,12 @@ import FlickPhotoset from '../FlickrPhotoset/FlickrPhotoset';
 
 const Gallery = () => (
   <div className="Gallery">
-    <FlickPhotoset photoset_id="72157623595640529"></FlickPhotoset>
-    <FlickPhotoset photoset_id="72157619267683452"></FlickPhotoset>
-    <FlickPhotoset photoset_id="72157619267747612"></FlickPhotoset>
-    <FlickPhotoset photoset_id="72157636629533746"></FlickPhotoset>
-    <FlickPhotoset photoset_id="72157658639295708"></FlickPhotoset>
-    <FlickPhotoset photoset_id="72157626117809198"></FlickPhotoset>
+    <FlickPhotoset api_key="ca88703a9cf7d3f3d78b90306a3a80c5" user_id="11413095@N00" photoset_id="72157623595640529" limit={6}></FlickPhotoset>
+    <FlickPhotoset api_key="ca88703a9cf7d3f3d78b90306a3a80c5" user_id="11413095@N00" photoset_id="72157619267683452" limit={6}></FlickPhotoset>
+    <FlickPhotoset api_key="ca88703a9cf7d3f3d78b90306a3a80c5" user_id="11413095@N00" photoset_id="72157619267747612" limit={6}></FlickPhotoset>
+    <FlickPhotoset api_key="ca88703a9cf7d3f3d78b90306a3a80c5" user_id="11413095@N00" photoset_id="72157658639295708" limit={6}></FlickPhotoset>
+    <FlickPhotoset api_key="ca88703a9cf7d3f3d78b90306a3a80c5" user_id="11413095@N00" photoset_id="72157619285112568" limit={6}></FlickPhotoset>
+    <FlickPhotoset api_key="ca88703a9cf7d3f3d78b90306a3a80c5" user_id="11413095@N00" photoset_id="72157626117809198" limit={6}></FlickPhotoset>
   </div>
 );
 
