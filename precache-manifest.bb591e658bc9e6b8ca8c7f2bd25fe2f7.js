2d5d8a1ed67b171a96c5107d560266457a42cd73
Alexis Rengifo <alexisrengifo@gmail.com>
2020-02-13 17:20:17 -0500

    Updates

---
+++ precache-manifest.bb591e658bc9e6b8ca8c7f2bd25fe2f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ab35d5ba9d745e1e7b62003a247d95",
+    "url": "/Simple-Navigation-with-React/index.html"
+  },
+  {
+    "revision": "d245e5fd31811b3af527",
+    "url": "/Simple-Navigation-with-React/static/css/main.7dca701f.chunk.css"
+  },
+  {
+    "revision": "38b8e44b001a320a5b94",
+    "url": "/Simple-Navigation-with-React/static/js/2.562d7362.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Simple-Navigation-with-React/static/js/2.562d7362.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d245e5fd31811b3af527",
+    "url": "/Simple-Navigation-with-React/static/js/main.2ad583e7.chunk.js"
+  },
+  {
+    "revision": "f7566ec5d27a776f0421",
+    "url": "/Simple-Navigation-with-React/static/js/runtime-main.ec66716f.js"
+  }
+]);
